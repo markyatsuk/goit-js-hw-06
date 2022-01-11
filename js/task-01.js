639796bad0e475b3be155adb91b1c82eb614e913
Mark Yatsiuk <markiatsuk@gmail.com>
2022-01-12 00:00:28 +0200

    comments

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,8 @@
-
+// создаем массив с элементами li.item
 const liItemsEl = Array.from(document.querySelectorAll('#categories>li.item')); 
- 
 console.log(`Number of categories: ${liItemsEl.length}.`);
 
+// проходимся по массиву, забираем текстовое содержимое заголовка и считаем колво дочерних элементов внутренних ul(количество элементов в категории)
 for(const item of liItemsEl){
     let title = item.firstElementChild.textContent;
     let quantityOfElements = item.lastElementChild.children.length;
